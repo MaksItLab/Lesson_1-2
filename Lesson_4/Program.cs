9c5ee654dee741b322f61f9b02238eff8ec68db6
MaksItLab <maksim.ardintsev@mail.ru>
2025-02-12 16:18:21 +0300

    Рассмотрели инициализацию и вывод массива

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -8,8 +8,7 @@ namespace Lesson_4
 {
     // 1. github
     // 2. массивы
-    // 3. вложенные циклы
-    // 4. операции с массивами :
+    // 3. операции с массивами :
     //      a. инициализация массива
     //      б. вывод массива
     //      в. заполнение массива с клавиатуры
@@ -17,11 +16,27 @@ namespace Lesson_4
     //      д. найти сумму четных чисел массива
     //      е. найти наименьший элемент массива
     //      ё. найти индекс элемента
+    // 4. вложенные циклы
     internal class Program
     {
         static void Main(string[] args)
         {
+            //int[] array = new int[10];
+            //int[] array; // объявление
+            //array = new int[10]; // инициализация
+            //int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            //int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7 ,8 };
+            int[] array = { -1, 23, 10, 0, -4, 5, 9 };
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
             
+
+
         }
     }
 

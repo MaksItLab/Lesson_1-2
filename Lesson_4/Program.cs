7c0334fe662e3a73d329a41bc148710fb950bd30
MaksItLab <maksim.ardintsev@mail.ru>
2025-02-12 15:53:01 +0300

    Создание урока 4

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    // 1. github
+    // 2. массивы
+    // 3. вложенные циклы
+    // 4. операции с массивами :
+    //      a. инициализация массива
+    //      б. вывод массива
+    //      в. заполнение массива с клавиатуры
+    //      г. вывод массива в обратном порядке
+    //      д. найти сумму четных чисел массива
+    //      е. найти наименьший элемент массива
+    //      ё. найти индекс элемента
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}

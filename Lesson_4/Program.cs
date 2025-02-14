3e82433a22f1e152222bee3bf6f5f3da6f797e97
MaksItLab <maksim.ardintsev@mail.ru>
2025-02-14 16:08:23 +0300

    Добавили методы нахождения: суммы четных элементов, минимального элемента и индекса

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -16,14 +16,16 @@ namespace Lesson_4
     //      д. найти сумму четных чисел массива
     //      е. найти наименьший элемент массива
     //      ё. найти индекс элемента
-    // 4. вложенные циклы
+    // 4. операторы break(останавливает цикл) и continue(начинает следующую итерацию)
+    // 5. двумерные массивы
+    // 6. зубчатые массивы
+    // 7. вложенные циклы
     internal class Program
     {
         static void Main(string[] args)
         {
             #region
             #endregion
-
 
             #region Инициализация
             //int[] array = new int[10];
@@ -41,25 +43,92 @@ namespace Lesson_4
             //}
             //Console.WriteLine();
             #endregion
-
             #region Ввод с клавиатуры
             //int[] array = new int[10];
             //array = FillArray(array);
             //PrintArray(array);
             #endregion
-
             #region Вывод в обратном порядке
-            int[] array = new int[10];
-            array = FillArray(array);
+            //int[] array = new int[10];
+            //array = FillArray(array);
 
 
-            for (int i = array.Length - 1; i >= 0;i--)
-            {
-                Console.Write(array[i] + " ");
-            }
-            
+            //for (int i = array.Length - 1; i >= 0;i--)
+            //{
+            //    Console.Write(array[i] + " ");
+            //}
+
+            #endregion
+            #region Нахождение суммы четных элементов
+            //int[] array = { 0, 2, 6, 3, 7, 5, 10, -12, 3, -11, 14 };
+
+            //int sum = SumEvenNumbers(array);
+            //Console.WriteLine("Сумма четных элементов: " + sum);
+            #endregion
+            #region Нахождение минимального элемента
+            //int[] array = { 5, 12, 19, 101, 0, 12 };
+
+            //int min = MinElement(array);
+            //Console.WriteLine("Минимальный элемент: " + min);
+            #endregion
+            #region Нахождение индекса
+            //int[] array = { 12, 2, 4, 5, 4, 5, 9, 3 };
+            //int index = IndexOfElement(array, 12);
+            //Console.WriteLine($"Индекс числа {12} = {index}");
             #endregion
 
+
+        }
+
+        public static int IndexOfElement(int[] array, int number)
+        {
+            int index = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                continue;
+                if (array[i] == number)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+
+        public static int MinElement(int[] array)
+        {
+            int min = array[0]; // хранится минимальное значение
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        public static int SumEvenNumbers(int[] array)
+        {
+            int sum = 0;
+            //int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    sum += array[i];
+                    //count++;
+                }
+            }
+
+            return sum;
         }
 
         public static int[] FillArray(int[] array)
